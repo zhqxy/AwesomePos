e6b3ea3f45d4e599427a99fd16526d3c456ca928
zhou <1576482718@qq.com>
2019-09-27 10:14:01 +0800

    add

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Pos from '@/components/Pos'
+import leftNav from '@/components/common/leftNav'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Pos',
+      component: Pos
+    }
+  ]
+})
